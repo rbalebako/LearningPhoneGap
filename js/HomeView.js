1ab8da0842586909beed88f6be4d64d5d7ba6650
rbalebako <me@rebeccahunt.com>
2014-01-10 11:51:55 -0500

    index and styles

--- js/HomeView.js
+++ js/HomeView.js
@@ -12,11 +12,18 @@ var HomeView = function(store) {
    };
     
 
-    this.findByName = function() {
-        store.findByName($('.search-key').val(), function(employees) {
-            $('.employee-list').html(HomeView.liTemplate(employees));
-        });
-    };
+   this.findByName = function() {
+    store.findByName($('.search-key').val(), function(employees) {
+        $('.employee-list').html(HomeView.liTemplate(employees));
+        if (self.iscroll) {
+            console.log('Refresh iScroll');
+            self.iscroll.refresh();
+        } else {
+            console.log('New iScroll');
+            self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false });
+        }
+    });
+};
         
     this.initialize();
 }
